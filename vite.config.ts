5ef63e9436c5a2dcf3409d86fad46db5aaaad848
Hassan <hassan.ali.36900@outlook.com>
2025-05-23 02:03:57 +0300

    fix: remove CORS configuration from Vite proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -20,20 +20,6 @@ export default defineConfig({
         target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
-        // this is set to allow for CORS requests
-        configure: (proxy) => {
-          proxy.on("proxyReq", (proxyReq) => {
-            proxyReq.setHeader("Access-Control-Allow-Origin", "*");
-            proxyReq.setHeader(
-              "Access-Control-Allow-Methods",
-              "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-            );
-            proxyReq.setHeader(
-              "Access-Control-Allow-Headers",
-              "Content-Type, Authorization",
-            );
-          });
-        },
       },
     },
   },
