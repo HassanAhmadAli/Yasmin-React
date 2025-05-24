217382187db0fbd745ba82aa87dc4d9b257369c0
Hassan <hassan.ali.36900@outlook.com>
2025-05-24 18:26:15 +0300

    added hover to images

--- src/pages/products/components/RealContent.tsx
+++ src/pages/products/components/RealContent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { toast } from "sonner";
-
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Product } from "../models/product";
 
@@ -92,49 +91,53 @@ const RealCellContent = ({
   isMobile: boolean;
   products: Array<Product>;
 }) => {
+  
   return (
     <div className="flex h-min flex-wrap gap-4 space-y-4">
       {products.map((product) => (
-        <>
-          <Card className="w-80 p-4">
-            <CardHeader>
-              <CardTitle className="text-lg">{product.title}</CardTitle>
-            </CardHeader>
+        <Card key={product._id} className="w-80 p-4">
+          <CardHeader>
+            <CardTitle className="text-lg">{product.title}</CardTitle>
+          </CardHeader>
 
-            <img src={product.image} />
-            <h2 className="flex flex-row-reverse">
-              <span className="text-sm">{product.category}</span>
-            </h2>
-            <h3 className="mb-2 text-sm">{product.description}</h3>
+          <img 
+            src={product.image} 
+            className="cursor-pointer hover:opacity-80 transition-opacity"
+            onClick={() => {}}
+            alt={product.title}
+          />
+          <h2 className="flex flex-row-reverse">
+            <span className="text-sm">{product.category}</span>
+          </h2>
+          <h3 className="mb-2 text-sm">{product.description}</h3>
 
-            <div className="mb-2 flex items-center gap-2">
-              <div className="flex">
-                {[1, 2, 3, 4, 5].map((i) =>
-                  product.rating.rate >= i ? (
-                    <Star
-                      key={i}
-                      className="h-3 w-3 fill-orange-400 text-orange-400"
-                    />
-                  ) : (
-                    <Star
-                      key={i}
-                      className="fill-white-400 h-3 w-3 text-orange-400"
-                    />
-                  ),
-                )}
-              </div>
-
-              <span className="text-sm text-blue-600">
-                {product.rating.count}
-              </span>
+          <div className="mb-2 flex items-center gap-2">
+            <div className="flex">
+              {[1, 2, 3, 4, 5].map((i) =>
+                product.rating.rate >= i ? (
+                  <Star
+                    key={i}
+                    className="h-3 w-3 fill-orange-400 text-orange-400"
+                  />
+                ) : (
+                  <Star
+                    key={i}
+                    className="fill-white-400 h-3 w-3 text-orange-400"
+                  />
+                ),
+              )}
             </div>
-            <RatingDialog />
-            <div className="mb-3 text-2xl">
-              ${product.price}
-              <sup className="text-lg">99</sup>
-            </div>
-          </Card>
-        </>
+
+            <span className="text-sm text-blue-600">
+              {product.rating.count}
+            </span>
+          </div>
+          <RatingDialog />
+          <div className="mb-3 text-2xl">
+            ${product.price}
+            <sup className="text-lg">99</sup>
+          </div>
+        </Card>
       ))}
     </div>
   );
