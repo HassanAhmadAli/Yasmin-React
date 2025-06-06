ba0113a969b2666188de4197c03ca0b539293c9b
Hassan <hassan.ali.36900@outlook.com>
2025-06-06 23:41:28 +0300

    added react-scan to visualize re-renders

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
+// src/index
+import { scan } from "react-scan";
 import React, { StrictMode } from "react";
+// using react-scan to highlite the parts of the UI that need optimization
+scan({
+  enabled: true,
+});
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "@/App.tsx";
