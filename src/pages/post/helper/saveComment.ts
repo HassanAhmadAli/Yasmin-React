9911b3294b79668bb147e3daf1d68cac42d13ee7
Hassan <hassan.ali.36900@outlook.com>
2025-06-14 01:30:36 +0300

    ok

--- src/pages/post/helper/saveComment.ts
+++ src/pages/post/helper/saveComment.ts
@@ -1,10 +1,13 @@
 import { useGlobalState } from "@/globalState";
 import { usePostPageState } from "../state"
-let counter = 0;
+import { Comment } from "@/model/comment";
+let fakeComment_ID = 0;
 export const saveComment = () => {
-    counter += 1;
+    fakeComment_ID += 1;
     const text = usePostPageState.getState().currentComment;
     const date = new Date(Date.now());
     const { name, email, _id } = useGlobalState.getState().user!;
-    usePostPageState.getState().addComment({ text, date, authorId: _id, authorName: name, _id: counter, email });
+    const comment: Comment = { text, date, authorId: _id, authorName: name, _id: fakeComment_ID, email };
+    console.log(comment);
+    usePostPageState.getState().addComment(comment);
 }
