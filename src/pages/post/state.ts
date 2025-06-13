fabd08f61edf5e7a8dcd991d1399a22793099e57
Hassan <hassan.ali.36900@outlook.com>
2025-06-13 17:59:42 +0300

    ok

--- src/pages/post/state.ts
+++ src/pages/post/state.ts
@@ -5,16 +5,20 @@ interface PostPageState {
   post: Post | null;
   isLoading: boolean;
   error: string | null;
+  comments: Comment[];
   setPost: (post: Post) => void;
   setIsLoading: (val: boolean) => void;
   setError: (error: string | null) => void;
+  addComment: (comment: Comment) => void;
 }
 
 export const usePostPageState = create<PostPageState>((set) => ({
   isLoading: true,
   post: null,
   error: null,
+  comments: [],
   setPost: (post: Post) => set({ post }),
   setIsLoading: (isLoading: boolean) => set({ isLoading }),
   setError: (error: string | null) => set({ error }),
+  addComment: (comment: Comment) => set(state => ({ comments: [...state.comments, comment] })),
 }));
