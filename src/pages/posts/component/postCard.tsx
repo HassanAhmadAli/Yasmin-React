44800ce556b4402599a469bebd6affd766f7fe0a
Hassan <hassan.ali.36900@outlook.com>
2025-06-13 17:26:27 +0300

    fixed a bug

--- src/pages/posts/component/postCard.tsx
+++ src/pages/posts/component/postCard.tsx
@@ -19,33 +19,36 @@ const extractWords = (text: string) => {
 };
 
 export function PostCard(
-  { _id, title, body, customer }: Post,
+  post: Post,
   navigate: NavigateFunction,
   onFavorite: () => void,
   isFavorite: boolean,
 ) {
   return (
-    <Card className="h-4xl flex w-[30%] min-w-[300px] flex-col bg-white/50 backdrop-blur-sm transition-all duration-300 hover:-translate-y-1 hover:bg-white/40 hover:shadow-xl">
+    <Card
+      key={post._id}
+      className="h-4xl flex w-[30%] min-w-[300px] flex-col bg-white/50 backdrop-blur-sm transition-all duration-300 hover:-translate-y-1 hover:bg-white/40 hover:shadow-xl"
+    >
       <CardHeader className="space-y-2 pb-3">
         <CardTitle className="text-primary/90 hover:text-primary h-15 line-clamp-2 text-xl font-bold">
-          {title}
+          {post.title}
         </CardTitle>
       </CardHeader>
       <CardContent className="flex-grow space-y-4">
         <p className="line-clamp-3 text-base text-gray-600/90">
-          {extractWords(body)}
+          {extractWords(post.body)}
         </p>
         <div className="mt-auto flex items-center gap-3 text-sm text-gray-500">
           <div className="relative">
             <div className="border-3 flex h-10 w-10 items-center justify-center rounded-full border-gray-300 bg-purple-600 font-semibold text-white">
-              {customer.name
+              {post.customer.name
                 .split(" ")
                 .map((n) => n[0])
                 .join("")}
             </div>
           </div>
           <span className="font-medium">
-            By <span className="text-primary/80">{customer.name}</span>
+            By <span className="text-primary/80">{post.customer.name}</span>
           </span>
         </div>
       </CardContent>
@@ -53,7 +56,7 @@ export function PostCard(
         <Button
           variant="ghost"
           size="sm"
-          onClick={() => navigate(`/post/${_id}`)}
+          onClick={() => navigate(`/post/${post._id}`)}
           className="hover:text-primary hover:bg-primary/5 flex items-center gap-2 transition-colors"
         >
           Read More <ArrowRight className="ml-1 h-4 w-4" />
